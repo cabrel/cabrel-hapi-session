bc393049e421fe09f0534411361fb5c4b4594eb6
Robin Harper <cabrel@zerklabs.com>
2014-02-10 12:16:26 -0500

    v1.0.4
    fixed package versions

--- test/session.js
+++ test/session.js
@@ -1,5 +1,5 @@
 var expect = require('chai').expect;
-var sessions = require('../');
+var sessions = require('../').scheme(null, { ironPassword: 'password123' });
 
 
 describe('cabrel-hapi-session', function() {
@@ -7,7 +7,7 @@ describe('cabrel-hapi-session', function() {
 
   describe('#storeSession', function() {
     it('returns a valid guid', function(done) {
-      sessions.storeSession({a: 1}, 'password123').done(function(result) {
+      sessions.storeSession({a: 1}).done(function(result) {
         expect(result).to.contain('-');
         sessionId = result;
         done();
@@ -17,7 +17,7 @@ describe('cabrel-hapi-session', function() {
 
   describe('#fetchSession', function() {
     it('returns session object', function(done) {
-      sessions.fetchSession(sessionId, 'password123').done(function(result) {
+      sessions.fetchSession(sessionId).done(function(result) {
         expect(result).to.be.an.instanceof(Object);
         expect(result.a).to.equal(1);
         done();
@@ -28,7 +28,7 @@ describe('cabrel-hapi-session', function() {
   describe('#removeSession', function() {
     it('removes a session', function(done) {
       sessions.removeSession(sessionId).then(function() {
-        return sessions.fetchSession(sessionId, 'password123');
+        return sessions.fetchSession(sessionId);
       }).done(null, function(error) {
         expect(error).to.be.an.instanceof(Error);
         done();
